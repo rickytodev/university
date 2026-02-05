4418533add1264c20b07ffc1de6e70d103b5a6f7
RicardoVegaG <rikytoloser@gmail.com>
2026-02-04 19:27:46 -0600

    Implement do-while loop for user input and calculate mean and mode of entered numbers

--- 2026-A/04-02/practica2.3/pract02.3_4 VegaGarcia RicardoJeshua.c
+++ 2026-A/04-02/practica2.3/pract02.3_4 VegaGarcia RicardoJeshua.c
@@ -9,5 +9,60 @@
 
 int main()
 {
+    int N;
+    int numeros[100];
+    int i = 0, j;
+    float suma = 0;
+    float media;
+    int moda = 0;
+    int maxFrecuencia = 0;
+
+    printf("Cuantos numeros desea ingresar: ");
+    scanf("%d", &N);
+
+    if (N <= 0)
+    {
+        printf("El numero de elementos debe ser mayor que cero.\n");
+        return 1;
+    }
+
+    do
+    {
+        printf("Numero %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+        suma += numeros[i];
+        i++;
+    } while (i < N);
+
+    media = suma / N;
+
+    i = 0;
+    do
+    {
+        int frecuencia = 0;
+        j = 0;
+
+        do
+        {
+            if (numeros[i] == numeros[j])
+                frecuencia++;
+            j++;
+        } while (j < N);
+
+        if (frecuencia > maxFrecuencia)
+        {
+            maxFrecuencia = frecuencia;
+            moda = numeros[i];
+        }
+
+        i++;
+    } while (i < N);
+
+    printf("\nMedia: %.2f\n", media);
+    if (maxFrecuencia > 1)
+        printf("Moda: %d\n", moda);
+    else
+        printf("No hay moda\n");
+
     return 0;
 }
